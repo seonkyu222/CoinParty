4b1ab86ad57b10e74f1ad73ffe4a338a22d4e00c
seoseol <qwejlk0203@naver.com>
2021-09-14 20:57:51 +0900

    edit board

--- app/src/main/java/com/example/codingparty/MainActivity.java
+++ app/src/main/java/com/example/codingparty/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.codingparty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LinearLayout layout_test = (LinearLayout) findViewById(R.id.ll_board_main);
+
+        layout_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), board_main.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
 }
