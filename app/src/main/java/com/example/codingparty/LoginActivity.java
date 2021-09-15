7ed596459c5763ba7cec1aa023ba148a949ad882
seonkyu222 <fineface02@gmail.com>
2021-09-15 20:25:57 +0900

    login

---
+++ app/src/main/java/com/example/codingparty/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.codingparty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText user_id;
+    private EditText user_pwd;
+    private Button login_btn;
+    boolean textCheck = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        user_id = findViewById(R.id.user_id);
+        user_pwd = findViewById(R.id.user_pwd);
+        login_btn = findViewById(R.id.button_login);
+
+        user_id.addTextChangedListener(loginTextWatcher);
+        user_pwd.addTextChangedListener(loginTextWatcher);
+    }
+
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String idInput = user_id.getText().toString().trim();
+            String pwdInput = user_pwd.getText().toString().trim();
+            if(!idInput.isEmpty() && !pwdInput.isEmpty()){
+                login_btn.setBackground(getResources().getDrawable(R.drawable.round_button_after));
+            }else {
+                login_btn.setBackground(getResources().getDrawable(R.drawable.round_button));
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+}
