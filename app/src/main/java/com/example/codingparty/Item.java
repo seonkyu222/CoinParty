bd56ad0b0d6a4536b42057555fa30f1a8537eb30
seoseol <qwejlk0203@naver.com>
2021-09-28 19:18:57 +0900

    muee

---
+++ app/src/main/java/com/example/codingparty/Item.java
@@ -0,0 +1,13 @@
+package com.example.codingparty;
+
+public class Item {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
