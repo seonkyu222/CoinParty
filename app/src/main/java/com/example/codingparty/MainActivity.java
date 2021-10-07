d3995449cd758ab250d2c460c86b8e7422eb6022
seoseol <qwejlk0203@naver.com>
2021-10-07 19:49:46 +0900

    fragment

--- app/src/main/java/com/example/codingparty/MainActivity.java
+++ app/src/main/java/com/example/codingparty/MainActivity.java
@@ -1,21 +1,61 @@
 package com.example.codingparty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toolbar;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
+
 
 public class MainActivity extends AppCompatActivity {
-    EditText user_pwd;
-    Button button_login;
-    Toolbar myToolbar;
+    Fragment menu1Fragment;
+    Fragment menu2Fragment;
+    Fragment menu3Fragment;
+    Fragment menu4Fragment;
+
+
+    private class Menu1Fragment extends Fragment {
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+
+            switch (item.getItemId()) {
+
+                case R.id.bottom_home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.activity_main,menu1Fragment).commit();
+                    return true;
+
+                case R.id.bottom_equipment:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.activity_main,menu2Fragment).commit();
+                    return true;
+
+                case R.id.bottom_list:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.activity_main,menu3Fragment).commit();
+                    return true;
+
+                case R.id.bottom_myPage:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.activity_main,menu4Fragment).commit();
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
 
 
     @SuppressLint("NewApi")
@@ -24,34 +64,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView navView = findViewById(R.id.bottom_navigation);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        setSupportActionBar(myToolbar);
+        menu1Fragment = new mainFragment(); //메인
+        menu2Fragment = new equipmentFragment(); //비품관리
+        menu3Fragment = new boardFragment(); //보드
+        menu4Fragment = new myPageFragment(); //마이페이지
 
 
-/*        user_pwd =  findViewById(R.id.user_pwd);
-        button_login=findViewById(R.id.button_login);
-        user_pwd.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (editable.length() > 0) {
-                    button_login.setClickable(true);
-//                    button_login.setBackgroundColor(Color.BLUE);
-                } else {
-                    button_login.setClickable(false);
-                    button_login.setBackgroundColor(Color.GRAY);
-                }
-            }
-        }); */
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.activity_main,menu1Fragment).commit();
 
     }
 
     private void setSupportActionBar(Toolbar myToolbar) {
     }
-
 }
