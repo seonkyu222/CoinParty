4973258c9634a7a9b8b8bd5b8526a3c4aba430d4
kjea123 <kjsk7898@gmail.com>
2021-11-01 19:00:34 +0900

    update

--- app/src/main/java/com/example/codingparty/MainActivity.java
+++ app/src/main/java/com/example/codingparty/MainActivity.java
@@ -1,26 +1,14 @@
 package com.example.codingparty;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-
 import android.annotation.SuppressLint;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toolbar;
 
-import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,12 +21,12 @@ public class MainActivity extends AppCompatActivity {
     private ListView list1;
     private ListView list2;
 
-
     @SuppressLint({"NewApi", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         list1 = (ListView) findViewById(R.id.list1);
         List<String> data = new ArrayList<>();
@@ -56,8 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void setSupportActionBar(Toolbar viewById) {
-    }
+
+
 
 }
 
