20a08a0621a105bf19311dad24037eb2e42af0c1
seoseol <qwejlk0203@naver.com>
2021-11-01 18:59:16 +0900

    hi

--- app/src/main/java/com/example/codingparty/board_main.java
+++ app/src/main/java/com/example/codingparty/board_main.java
@@ -5,31 +5,45 @@ import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
 
 public class board_main extends AppCompatActivity {
 
     private class Menu2Fragment extends Fragment {
     }
 
+    private LinearLayout layout_suggest;
+    Button testt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.e(TAG, "InBoardMain");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_main);
 
-        LinearLayout layout_free = (LinearLayout) findViewById(R.id.ll_free);
+        layout_suggest = (LinearLayout) findViewById(R.id.ll_suggest);
 
-        layout_free.setOnClickListener(new View.OnClickListener() {
+        layout_suggest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), activity_board_free.class);
-                startActivity(intent);
+
             }
         });
 
+        layout_suggest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "ininininin");
+                Intent intent = new Intent(getApplicationContext(), BoardSuggest.class);
+                startActivity(intent);
 
-
+            }
+        });
     }
 }
