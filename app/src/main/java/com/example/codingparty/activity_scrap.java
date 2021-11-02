b7a3d30f3c946b52d8afed511a97398b9de3db3d
seonkyu222 <fineface02@gmail.com>
2021-11-02 19:25:19 +0900

    SDAFd

--- app/src/main/java/com/example/codingparty/activity_scrap.java
+++ app/src/main/java/com/example/codingparty/activity_scrap.java
@@ -12,17 +12,17 @@ import java.util.List;
 
 public class activity_scrap extends AppCompatActivity {
 
-//    private ListView listView;
-//    private ScrapListAdapter adapter;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_scrap);
-//        listView = (ListView) findViewById(R.id.list);
-//        adapter = new ScrapListAdapter(activity_scrap.this);
-//        listView.setAdapter(adapter);
-//
-//        adapter.notifyDataSetChanged();
-//    }
+    private ListView listView;
+    private ScrapListAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrap);
+        listView = (ListView) findViewById(R.id.list);
+        adapter = new ScrapListAdapter(activity_scrap.this);
+        listView.setAdapter(adapter);
+
+        adapter.notifyDataSetChanged();
+    }
 }
