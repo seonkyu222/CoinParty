9420015ae0a88ef4a56e88cf1b6e83566e28dcd7
seonkyu222 <fineface02@gmail.com>
2021-11-09 20:42:40 +0900

    df

--- app/src/main/java/com/example/codingparty/activity_mypage.java
+++ app/src/main/java/com/example/codingparty/activity_mypage.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class activity_mypage extends AppCompatActivity {
 
     Button test;
+    TextView text;
 
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
@@ -18,6 +20,15 @@ public class activity_mypage extends AppCompatActivity {
         setContentView(R.layout.activity_mypage);
 
         test = (Button) findViewById(R.id.scrap);
+        text = (TextView) findViewById(R.id.option);
+
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Mypage_Option.class);
+                startActivity(intent);
+            }
+        });
 
         test.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,8 +40,8 @@ public class activity_mypage extends AppCompatActivity {
 
     }
 
-    public void onClick(View view){
-        Intent intent = new Intent(this, Mypage_Scrap.class);
-        startActivity(intent);
-    }
+//    public void onClick(View view){
+//        Intent intent = new Intent(this, Mypage_Scrap.class);
+//        startActivity(intent);
+//    }
 }
