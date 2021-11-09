8ae1210048f8e78bdf3f65159c93f13c59972b4c
seoseol <qwejlk0203@naver.com>
2021-11-09 20:40:22 +0900

    11.09 2222

--- app/src/main/java/com/example/codingparty/fragmentBoard.java
+++ app/src/main/java/com/example/codingparty/fragmentBoard.java
@@ -7,12 +7,16 @@ import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -31,6 +35,9 @@ public class fragmentBoard extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         Log.i(TAG,"bf_onCreate");
         View view = inflater.inflate(R.layout.activity_board_main, container, false);
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) view.findViewById(R.id.ll_study);
+        constraintLayout.setOnClickListener(this);
 
         LinearLayout layoutContest = (LinearLayout) view.findViewById(R.id.ll_contest);
         layoutContest.setOnClickListener(this::onClick);
@@ -54,6 +61,33 @@ public class fragmentBoard extends Fragment implements View.OnClickListener {
 
         switch (v.getId()) {
 
+            case R.id.ll_study:
+                Log.i(TAG, "in study");
+// 잉 아 몰라,,, 몰라몰라,,, (._.),,,
+//                final PopupMenu popupMenu = new PopupMenu(getActivity().getApplicationContext(), view);
+//                popupMenu.getMenuInflater().inflate(R.menu.popup,popupMenu.getMenu());
+//                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener(){
+//
+//                    @Override
+//                    public boolean onMenuItemClick(MenuItem item) {
+//
+//                        switch (v.getId()){
+//                            case R.id.pop_menu1:
+//                                Log.i(TAG, "팝업 메뉴 1 클릭");
+//
+//                            case R.id.pop_menu2:
+//
+//                            case R.id.pop_menu3:
+//
+//                            case R.id.pop_menu4:
+//                        }
+//
+//                        return true;
+//                    }
+//                });
+//                popupMenu.show();
+                break;
+
             case R.id.ll_contest:
                 Intent intent_c = new Intent(getActivity().getApplicationContext(), BoardContest.class);
                 startActivity(intent_c);
@@ -75,10 +109,6 @@ public class fragmentBoard extends Fragment implements View.OnClickListener {
                 break;
         }
 
-
-
     }
-
-
 
 }
